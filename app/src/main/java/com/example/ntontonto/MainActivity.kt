11e9aad32831af62560a5d4578449b8f68dd88e2
kaedeh1ra <147055224+kaedeh1ra@users.noreply.github.com>
2024-12-05 17:06:27 +0400

    ура

--- app/src/main/java/com/example/ntontonto/MainActivity.kt
+++ app/src/main/java/com/example/ntontonto/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ntontonto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding = ActivityMainBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val view = binding.root
+        setContentView(view)
     }
 }
